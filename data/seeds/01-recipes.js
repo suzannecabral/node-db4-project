4815e5cad1015b752cbf88aeda504c4e71cb876c
Suzanne Cabral <suzannecabral@live.com>
2020-11-05 19:08:27 -0800

    All data seeded successfully, sql views work

--- data/seeds/01-recipes.js
+++ data/seeds/01-recipes.js
@@ -1,13 +1,14 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('recipes').truncate()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('recipes').insert([
+        {name: 'Triple the Ginger Cookies'},
+        {name: 'Pumpkin Fluff Dip'},
+        {name:'Spiced Orange Cranberry Sauce'},
+        {name: 'Beef Stew with Ale'},
       ]);
     });
 };
