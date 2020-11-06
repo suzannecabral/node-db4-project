7595ddbf2b2841e023dd5b8204af1c1ebc45c00b
Suzanne Cabral <suzannecabral@live.com>
2020-11-05 20:06:01 -0800

    getShoppingList and endpoint work

--- recipes/recipesModel.js
+++ recipes/recipesModel.js
@@ -6,6 +6,19 @@ module.exports = {
 
     return db('recipes');
   },
-  getShoppingList(recipe_id){},
+  async getShoppingList(recipe_id){
+    //make sure recipe exists
+    const foundRecipe = await db('recipes').where('id',recipe_id);
+
+    if(foundRecipe){
+      return db('recipe_ingredients as ri')
+        .join('recipes as r', 'ri.recipe_id', 'r.id')
+        .join('ingredients as i', 'ri.ingredient_id', 'i.id')
+        .select('i.name as Ingredient', 'ri.ingredientQty as Amount')
+        .where('ri.recipe_id',recipe_id)
+    }else{
+      return Promise.resolve(null);
+    }
+  },
   getInstructions(recipe_id){}
 };
--- recipes/recipesRouter.js
+++ recipes/recipesRouter.js
@@ -15,5 +15,17 @@ router.get('/',(req,res,next)=>{
     });
 });
 
+// :8000/api/recipes/:id/shoppingList
+router.get('/:id/shoppingList',(req,res,next)=>{
+  const { id } = req.params;
+  Recipes.getShoppingList(id)
+    .then(data=>{
+      res.status(200).json(data)
+    })
+    .catch(err=>{
+      res.status(500).json({error:err})
+    });
+});
+
 
 module.exports = router;
